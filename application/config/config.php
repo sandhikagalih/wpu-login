e0e0f24109cc29109b09c2287c490e02cf120b11
Hamdanms <Hamdanjhee21@gmail.com>
2020-01-15 21:04:18 +0800

    konfigurasi awal database dan base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/wpu-login/';
+$config['base_url'] = 'http://localhost/wpu-login-sandikag/';
 
 /*
 |--------------------------------------------------------------------------
