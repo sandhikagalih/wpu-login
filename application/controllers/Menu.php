d3db643e501faefbeb5bb5fde85925e5002807ac
Hamdanms <Hamdanjhee21@gmail.com>
2020-01-15 21:26:19 +0800

    controllers/menu.php = - menambahkan fungsi edit dan hapus
    models/menu_model.php =
    - menambahkan fungsi saveSubMenu
    - menambahkan fungsi deleteSubMenu

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -7,6 +7,7 @@ class Menu extends CI_Controller
     {
         parent::__construct();
         is_logged_in();
+        $this->load->model('Menu_model', 'menu');
     }
 
     public function index()
@@ -66,9 +67,6 @@ class Menu extends CI_Controller
         }
     }
 
-
-
-    
     public function editMenu($id)
     {
         $this->db->update('user_menu', ['menu' => $this->input->post('menu') ] ,[ 'id' => $id ]);
@@ -82,5 +80,20 @@ class Menu extends CI_Controller
         $this->db->delete('user_sub_menu',['menu_id' => $id]); 
         $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">The menu has ben deleted!</div>');
         redirect('menu');
+    }
+    
+    public function editSubMenu($id)
+    {
+        $this->menu->saveSubMenu($id);
+
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">The menu has ben edited!</div>');
+        redirect('menu/submenu');
+    }
+    
+    public function deleteSubMenu($id)
+    {
+        $this->menu->deleteSubMenu($id);
+        $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">The menu has ben deleted!</div>');
+        redirect('menu/submenu');
     }
 }
