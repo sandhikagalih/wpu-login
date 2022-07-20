9a3e3c68a8611bf88a9763a08ea7effa2f68d783
1stGuardian <ranmarurega@gmail.com>
2022-07-20 19:27:48 +0700

    Edit user/index.php

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -4,16 +4,18 @@
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
-    <div class="row">
-        <div class="col-lg-8">
-            <?= $this->session->flashdata('message'); ?>
+    <?php if ($this->session->flashdata('message') == 'edited') : ?>
+        <div class="row">
+            <div class="col-lg-8">
+                <div class="alert alert-success" role="alert">Your profile has been edited!</div>
+            </div>
         </div>
-    </div>
+    <?php endif; ?>
 
-    <div class="card mb-3 col-lg-8">
+    <div class="card mb-3 border-0">
         <div class="row no-gutters">
-            <div class="col-md-4">
-                <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="card-img">
+            <div class="col-md-4 d-flex justify-content-center">
+                <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" alt="<?= $user['name']; ?>">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
@@ -29,4 +31,4 @@
 <!-- /.container-fluid -->
 
 </div>
-<!-- End of Main Content --> 
+<!-- End of Main Content -->
