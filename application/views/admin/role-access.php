a4e43a7d69c576eb6f9b40f046b97fe25729d2fb
1stGuardian <ranmarurega@gmail.com>
2022-07-20 15:50:40 +0700

    Edit admin views

--- application/views/admin/role-access.php
+++ application/views/admin/role-access.php
@@ -7,12 +7,16 @@
     <div class="row">
         <div class="col-lg-6">
 
-            <?= $this->session->flashdata('message'); ?>
+            <?php if ($this->session->flashdata('message') == 'changed') : ?>
+                <div class="alert alert-success" role="alert">
+                    Access Changed!
+                </div>
+            <?php endif; ?>
 
             <h5>Role : <?= $role['role']; ?></h5>
 
-            <table class="table table-hover">
-                <thead>
+            <table class="table table-hover table-striped">
+                <thead class="thead-dark">
                     <tr>
                         <th scope="col">#</th>
                         <th scope="col">Menu</th>
@@ -20,30 +24,26 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <?php $i = 1; ?>
-                    <?php foreach ($menu as $m) : ?>
-                    <tr>
-                        <th scope="row"><?= $i; ?></th>
-                        <td><?= $m['menu']; ?></td>
-                        <td>
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" <?= check_access($role['id'], $m['id']); ?> data-role="<?= $role['id']; ?>" data-menu="<?= $m['id']; ?>">
-                            </div>
-                        </td>
-                    </tr>
-                    <?php $i++; ?>
+                    <?php $i = 1;
+                    foreach ($menus as $m) : ?>
+                        <?php if ($m['id'] == 1) continue; ?>
+                        <tr>
+                            <th scope="row"><?= $i++; ?></th>
+                            <td class="text-capitalize"><?= $m['menu']; ?></td>
+                            <td>
+                                <div class="form-check">
+                                    <input type="checkbox" class="form-check-input" <?= check_access($role['id'], $m['id']); ?> data-role="<?= $role['id']; ?>" data-menu="<?= $m['id']; ?>">
+                                </div>
+                            </td>
+                        </tr>
                     <?php endforeach; ?>
                 </tbody>
             </table>
-
-
         </div>
     </div>
-
-
 
 </div>
 <!-- /.container-fluid -->
 
 </div>
-<!-- End of Main Content -- >     
+<!-- End of Main Content -->
--- application/views/admin/role.php
+++ application/views/admin/role.php
@@ -6,14 +6,11 @@
 
     <div class="row">
         <div class="col-lg-6">
-            <?= form_error('menu', '<div class="alert alert-danger" role="alert">', '</div>'); ?>
 
-            <?= $this->session->flashdata('message'); ?>
+            <a href="" class="btn btn-primary mb-3 float-right" data-toggle="modal" data-target="#newRoleModal">Add New Role</a>
 
-            <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#newRoleModal">Add New Role</a>
-
-            <table class="table table-hover">
-                <thead>
+            <table class="table table-hover table-striped">
+                <thead class="thead-dark">
                     <tr>
                         <th scope="col">#</th>
                         <th scope="col">Role</th>
@@ -21,27 +18,22 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <?php $i = 1; ?>
-                    <?php foreach ($role as $r) : ?>
-                    <tr>
-                        <th scope="row"><?= $i; ?></th>
-                        <td><?= $r['role']; ?></td>
-                        <td>
-                            <a href="<?= base_url('admin/roleaccess/') . $r['id']; ?>" class="badge badge-warning">access</a>
-                            <a href="" class="badge badge-success">edit</a>
-                            <a href="" class="badge badge-danger">delete</a>
-                        </td>
-                    </tr>
-                    <?php $i++; ?>
+                    <?php $i = 1;
+                    foreach ($role as $r) : ?>
+                        <tr>
+                            <th scope="row"><?= $i++; ?></th>
+                            <td class="text-capitalize"><?= $r['role']; ?></td>
+                            <td>
+                                <a href="<?= base_url('admin/roleaccess/') . $r['id']; ?>" class="badge badge-warning">Access</a>
+                                <a href="#" class="badge badge-primary" onclick="alert('Features temporarily inaccessible')">Edit</a>
+                                <a href="#" class="badge badge-danger" onclick="alert('Features temporarily inaccessible')">Delete</a>
+                            </td>
+                        </tr>
                     <?php endforeach; ?>
                 </tbody>
             </table>
-
-
         </div>
     </div>
-
-
 
 </div>
 <!-- /.container-fluid -->
@@ -50,10 +42,8 @@
 <!-- End of Main Content -->
 
 <!-- Modal -->
-
-<!-- Modal -->
-<div class="modal fade" id="newRoleModal" tabindex="-1" role="dialog" aria-labelledby="newRoleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+<div class="modal fade" id="newRoleModal" tabindex="-1" aria-labelledby="newRoleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="newRoleModalLabel">Add New Role</h5>
@@ -61,17 +51,17 @@
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <form action="<?= base_url('admin/role'); ?>" method="post">
-                <div class="modal-body">
+            <div class="modal-body">
+                <form action="<?= base_url('admin/role'); ?>" method="POST">
                     <div class="form-group">
                         <input type="text" class="form-control" id="role" name="role" placeholder="Role name">
                     </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Add</button>
-                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Add</button>
+            </div>
             </form>
         </div>
     </div>
-</div> 
+</div>
