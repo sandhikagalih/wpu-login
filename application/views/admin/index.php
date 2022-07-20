f1dd1127e0a80031a5fe3bf033832c7f5f327f62
1stGuardian <ranmarurega@gmail.com>
2022-07-20 15:45:57 +0700

    remove unnecesary space admin/index.php

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -4,10 +4,8 @@
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
-
-
 </div>
 <!-- /.container-fluid -->
 
 </div>
-<!-- End of Main Content --> 
+<!-- End of Main Content -->
