0575471ee58a991712a09e1c1ef0e65a0473bd55
1stGuardian <ranmarurega@gmail.com>
2022-07-20 19:26:38 +0700

    Edit how flashdata written in user/edit.php

--- application/views/user/edit.php
+++ application/views/user/edit.php
@@ -4,11 +4,18 @@
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
+    <?php if ($this->session->flashdata('message')) : ?>
+        <div class="row">
+            <div class="col-lg-8">
+                <div class="alert alert-danger" role="alert"><?= $this->session->flashdata('message'); ?></div>
+            </div>
+        </div>
+    <?php endif; ?>
 
     <div class="row">
         <div class="col-lg-8">
 
-            <?= form_open_multipart('user/edit'); ?>
+            <?php echo form_open_multipart('user/edit'); ?>
             <div class="form-group row">
                 <label for="email" class="col-sm-2 col-form-label">Email</label>
                 <div class="col-sm-10">
@@ -16,7 +23,7 @@
                 </div>
             </div>
             <div class="form-group row">
-                <label for="name" class="col-sm-2 col-form-label">Full name</label>
+                <label for="name" class="col-sm-2 col-form-label">Full Name</label>
                 <div class="col-sm-10">
                     <input type="text" class="form-control" id="name" name="name" value="<?= $user['name']; ?>">
                     <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
@@ -27,35 +34,39 @@
                 <div class="col-sm-10">
                     <div class="row">
                         <div class="col-sm-3">
-                            <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="img-thumbnail">
+                            <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="img-thumbnail border-0">
                         </div>
                         <div class="col-sm-9">
-                            <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="image" name="image">
-                                <label class="custom-file-label" for="image">Choose file</label>
+                            <div class="row">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" id="image" name="image">
+                                    <label class="custom-file-label" for="Image">Choose file</label>
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="1" id="deletepic" name="deletePic">
+                                    <label class="form-check-label" for="deletepic">
+                                        Delete current picture
+                                    </label>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
-            <div class="form-group row justify-content-end">
-                <div class="col-sm-10">
-                    <button type="submit" class="btn btn-primary">Edit</button>
+            <div class="form-group row mt-5">
+                <div class="col">
+                    <button type="submit" class="btn btn-primary">Save</button>
                 </div>
             </div>
-
-
             </form>
-
 
         </div>
     </div>
-
-
 
 </div>
 <!-- /.container-fluid -->
 
 </div>
-<!-- End of Main Content --> 
+<!-- End of Main Content -->
