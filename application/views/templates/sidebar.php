833664a0b4715293e76d57b208c0e7568364c915
1stGuardian <ranmarurega@gmail.com>
2022-07-23 13:06:53 +0700

    Edit logo's href in sidebar

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -2,7 +2,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url(); ?>">
         <div class="sidebar-brand-icon">
             <i class="fas fa-code"></i>
         </div>
